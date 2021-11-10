4518810b50d3ac0a16df807c8b40877bf4f75a4a
Anthony Xavier <anthony-xavier@live.com>
2021-11-10 15:51:41 +1000

    Added course files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/
+      }
+    ],
+  },
+};

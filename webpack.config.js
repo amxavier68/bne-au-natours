591c9617689c79394bb352e3c977eb0f250ed57f
Anthony Xavier <anthony-xavier@live.com>
2021-12-02 14:41:07 +1000

    Arbitrary commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  target: 'node',
+
+  entry: './App.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[contenthash].js',
+    libraryTarget: 'commonjs2',
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
